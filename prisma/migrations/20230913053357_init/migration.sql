47a49b67d3e5833cf8a126d5c1155038f7da99f0
Pearl-123 <pearlnerijean.calape@bisu.edu.ph>
2023-09-13 13:35:17 +0800

    updated migration with relationship

--- prisma/migrations/20230913053357_init/migration.sql
+++ prisma/migrations/20230913053357_init/migration.sql
@@ -1,11 +1,87 @@
-/*
-  Warnings:
+-- CreateTable
+CREATE TABLE `Shoe` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `name` VARCHAR(191) NOT NULL,
+    `description` VARCHAR(191) NULL,
+    `type` VARCHAR(191) NOT NULL,
+    `brand` VARCHAR(191) NOT NULL,
+    `price` DOUBLE NOT NULL,
+    `totalStocks` INTEGER NOT NULL,
+    `remainingStocks` INTEGER NOT NULL,
+    `imageUrl` VARCHAR(191) NOT NULL,
 
-  - You are about to drop the `shoecolor` table. If the table is not empty, all the data it contains will be lost.
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
-*/
--- DropTable
-DROP TABLE `shoecolor`;
+-- CreateTable
+CREATE TABLE `Type` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `name` VARCHAR(191) NOT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `Brand` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `name` VARCHAR(191) NOT NULL,
+    `logoUrl` VARCHAR(191) NOT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `User` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `fullName` VARCHAR(191) NOT NULL,
+    `email` VARCHAR(191) NOT NULL,
+    `password` VARCHAR(191) NOT NULL,
+
+    UNIQUE INDEX `User_email_key`(`email`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `Color` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `name` VARCHAR(191) NOT NULL,
+    `hex` VARCHAR(191) NOT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `Size` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `size` VARCHAR(191) NOT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `Category` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `name` VARCHAR(191) NOT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `Order` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `user` VARCHAR(191) NOT NULL,
+    `total` DOUBLE NOT NULL,
+    `addressStreet` VARCHAR(191) NOT NULL,
+    `addressCity` VARCHAR(191) NOT NULL,
+    `addressZipCode` VARCHAR(191) NOT NULL,
+    `paymentCardNumber` VARCHAR(191) NOT NULL,
+    `paymentExpiry` VARCHAR(191) NOT NULL,
+    `paymentCvv` VARCHAR(191) NOT NULL,
+    `isPlaced` BOOLEAN NOT NULL DEFAULT false,
+    `datePlaced` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
 -- CreateTable
 CREATE TABLE `ShoeOrder` (
